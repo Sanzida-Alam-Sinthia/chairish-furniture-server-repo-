acc53f8b391b67af3c29437f83e5b3e6207f00a7
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2022-11-26 18:48:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+

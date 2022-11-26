1db97b7cfb6bd26d4cb98a9bc18c53676d117077
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2022-11-26 23:44:39 +0600

    removed node modules and .env

--- index.js
+++ index.js
@@ -10,5 +10,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhcmclk.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+app.get('/', async (req, res) => {
+    res.send('resell server is running');
+})
+
+app.listen(port, () => console.log(`resell server running on ${port}`))
 

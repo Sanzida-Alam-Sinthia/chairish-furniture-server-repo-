49542d7b0207b287fae5f7f2c96f04b27318d058
Sanzida-Alam-Sinthia <sanzidaalamsinthia@gmail.com>
2022-11-27 22:55:35 +0600

    user and bookings added

--- index.js
+++ index.js
@@ -12,11 +12,27 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hhcmclk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function verifyJWT(req, res, next) {
 
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 async function run() {
     try {
         const productsCollection = client.db('resellFurniture').collection('products');
         const bookingsCollection = client.db('resellFurniture').collection('bookings');
+        const usersCollection = client.db('resellFurniture').collection('users');
 
         app.get('/products', async (req, res) => {
             console.log(req.query.category_id)
@@ -51,6 +67,23 @@ async function run() {
             const result = await bookingsCollection.insertOne(booking)
             res.send(result)
         })
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
 
 
 
